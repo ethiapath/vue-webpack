98713e423a0fe41e34ec02cb8b87a21f5a127d42
Hargobind <ethaipath@gmail.com>
2020-05-23 20:28:35 -0700

    Move vue root to app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import Vue from "vue";
-import HelloComponent from "./components/Hello.vue";
-import HelloDecoratorComponent from "./components/HelloDecorator.vue";
+import App from "./App"
 // import "./assets/neomorph.css";
 
 let v = new Vue({
     el: "#app",
     template: `
-    <div>
-        Name: <input v-model="name" type="text">
-        <h1>Hello Component</h1>
-        <hello-component :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Decorator Component</h1>
-        <hello-decorator-component :name="name" :initialEnthusiasm="5" />
-        </div>
+    <div class="App-Index">
+        <App />
+    </div>
     `,
     data: { name: "World" },
     components: {
-        HelloComponent,
-        HelloDecoratorComponent
+        App
     }
 });
